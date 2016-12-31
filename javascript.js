33c164035f878dbb57463acfb1fa9f19bbdd07c9
JavaScriptErika <ErikaJRitter@gmail.com>
2016-12-31 16:15:42 -0500

    Refactor submit/click events for form/button

--- javascript.js
+++ javascript.js
@@ -3,24 +3,19 @@
 //Radio button for Fahrenheit
 var radioF = $("#F");
 $(radioF).attr("checked", true);
+
 //answer message displays in calculation() or invalid()
 var answer = $("#answer");
 
-$("#enterKeyboard").on({
-    //Triggers event when user submits button from hitting enter on keyboard
-    submit: function() {
-        event.preventDefault();
-        submitButton();
-    }
+//User can submit from keyboard by pressing enter or clicking the submit button
+$("#enterKeyboard").submit(function(event) {
+    event.preventDefault();
+    $("#submitButton").click();
+    submitButton();
 });
 
 //Removes .redBorder if invalid from previous input when user clicks or presses keys  in input field
 $("#chirps").on("click keydown", resetBorder);
-
-//Triggers event when user submits button from clicking mouse
-$("#submitButton").click(function() {
-    submitButton();
-});
 
 //Grabs input value from user, declares variables for access to functions called in it
 function submitButton() {
@@ -28,11 +23,11 @@ function submitButton() {
     var cricketInput = +$("#chirps").val();
     //Clears input field
     $("#chirps").val("");
-    //Passing in cricketInput and calculation() parameters to call invalid function to first check if input is invalid, and if it is not, to perform the calculation()
+    //Passing in cricketInput and calculation() aeguments to call invalid function to first check if input is invalid, and if it is not, to perform the calculation()
     invalid(cricketInput, calculation);
 }
 
-//1st argument is cricketInput value, second is calculation() passed as callback function
+//1st parameter is cricketInput value, second is calculation() passed as callback function
 function invalid(cricketNumb, convertChirps) {
     //Checks if number is < 1 or a float as this is invalid data
     if (cricketNumb < 1 || cricketNumb % 1 !== 0) {
@@ -42,9 +37,9 @@ function invalid(cricketNumb, convertChirps) {
         $(answer).html("");
         //if the cricketStr is valid input - ie >= 1 and not a float, calculation function will execute to convert chirps to temp...
     } else {
-        //Checks if convertChirps is a function, and will execute if so
+        //Checks if convertChirps is a function
         if (typeof convertChirps === "function") {
-            //cricketInput is passed as an arugment named cricketStr, and the calculation function will execute
+            //cricketNumb is passed as an arugment (value of cricketInput), and the calculation function will execute
             convertChirps(cricketNumb);
         }
     }
@@ -57,7 +52,7 @@ function calculation(convertCricketNumb) {
     var cTempRounded = Math.round(((fTemp - 32) * .5556) * 10) / 10;
     //output variable text for calculation
     var message = "The crickets predict the temperature is about ";
-    //checks if Fahrenheit is checked -  the radio button's default (from html)
+    //checks if Fahrenheit is checked -  the radio button's default 
     if (radioF.is(':checked')) {
         //Output of Fahrenheit calculation
         message += fTemp + "&deg;F";
