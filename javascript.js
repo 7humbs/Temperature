d9cec20a6c2c6e31e225a3900a39c85a9497c3ac
JavaScriptErika <ErikaJRitter@gmail.com>
2017-01-10 03:37:22 -0500

    Rename/cache variables and form id

--- javascript.js
+++ javascript.js
@@ -1,45 +1,46 @@
 'use strict';
-
+//Input id
+var $chirps = $("#chirps");
 //Radio button for Fahrenheit
-var radioF = $("#F");
-$(radioF).attr("checked", true);
+var $radioF = $("#F");
+$($radioF).attr("checked", true);
 
-//answer message displays in calculation() or invalid()
-var answer = $("#answer");
+//$answer message displays in calculation() or invalid()
+var $answer = $("#answer");
 
 //User can submit from keyboard by pressing enter or clicking the submit button
-$("#enterKeyboard").submit(function(event) {
+$("#cricketForm").submit(function(event) {
     event.preventDefault();
     $("#submitButton").click();
     submitButton();
 });
 
 //Removes .redBorder if invalid from previous input when user clicks or presses keys  in input field
-$("#chirps").on("click keydown", resetBorder);
+$chirps.on("click keydown", resetBorder);
 
 //Grabs input value from user, declares variables for access to functions called in it
 function submitButton() {
     //  Get string from input field, converts to number
-    var cricketInput = +$("#chirps").val();
+    var $cricketInput = +$chirps.val();
     //Clears input field
-    $("#chirps").val("");
-    //Passing in cricketInput and calculation() aeguments to call invalid function to first check if input is invalid, and if it is not, to perform the calculation()
-    invalid(cricketInput, calculation);
+    $chirps.val("");
+    //Passing in $cricketInput and calculation() aeguments to call invalid function to first check if input is invalid, and if it is not, to perform the calculation()
+    invalid($cricketInput, calculation);
 }
 
-//1st parameter is cricketInput value, second is calculation() passed as callback function
+//1st parameter is $cricketInput value, second is calculation() passed as callback function
 function invalid(cricketNumb, convertChirps) {
     //Checks if number is < 1 or a float as this is invalid data
     if (cricketNumb < 1 || cricketNumb % 1 !== 0) {
         //highlights the input field red to signal invalid input
-        $("#chirps").addClass("redBorder");
-        //Answer is an empty string, this also resets the answer from previous answers if they were valid
-        $(answer).html("");
-        //if the cricketStr is valid input - ie >= 1 and not a float, calculation function will execute to convert chirps to temp...
+        $chirps.addClass("redBorder");
+        //$answer is an empty string, this also resets the $answer from previous answers if they were valid
+        $($answer).html("");
+        //if the cricketStr is valid input - ie >= 1 and not a float, calculation function will execute to convert $chirps to temp...
     } else {
         //Checks if convertChirps is a function
         if (typeof convertChirps === "function") {
-            //cricketNumb is passed as an arugment (value of cricketInput), and the calculation function will execute
+            //cricketNumb is passed as an arugment (value of $cricketInput), and the calculation function will execute
             convertChirps(cricketNumb);
         }
     }
@@ -52,23 +53,23 @@ function calculation(convertCricketNumb) {
     var cTempRounded = Math.round(((fTemp - 32) * .5556) * 10) / 10;
     //output variable text for calculation
     var message = "The crickets predict the temperature is about ";
-    //checks if Fahrenheit is checked -  the radio button's default 
-    if (radioF.is(':checked')) {
+    //checks if Fahrenheit is checked -  the radio button's default
+    if ($radioF.is(':checked')) {
         //Output of Fahrenheit calculation
         message += fTemp + "&deg;F";
-        $(answer).html(message);
+        $($answer).html(message);
 
         //Celcius calculation
     } else {
         //output of Celsius calculation
         message += cTempRounded + "&deg;C";;
-        $(answer).html(message);
+        $($answer).html(message);
     }
 }
 
 // Removes .redBorder and is called in event handlers
 function resetBorder() {
-    if ($("#chirps").hasClass("redBorder")) {
-        $("#chirps").removeClass("redBorder");
+    if ($chirps.hasClass("redBorder")) {
+        $chirps.removeClass("redBorder");
     }
 }
